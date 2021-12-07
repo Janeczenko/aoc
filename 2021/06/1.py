a641e2d4f213906af5fd09f25e7cead8ebc3262c
Janeczenko <janek.wyrzykowski@gmail.com>
2021-12-07 17:47:04 +0100

    Day 6 completed

---
+++ 2021/06/1.py
@@ -0,0 +1,21 @@
+import collections
+from typing import Counter
+
+with open("2021/06/input.txt", "r") as f:
+    data = [int(x) for x in f.read().strip().split(",")]
+
+fish = dict(Counter(data))
+print(fish)
+
+for _ in range(80):
+    fish_tmp = {}
+    for x in fish.keys():
+        if x == 0:
+            fish_tmp[8] = fish[x]
+            fish_tmp[6] = fish[x] if 6 not in fish_tmp.keys() else fish_tmp[6] + fish[x]
+        else:
+            fish_tmp[x - 1] = fish[x] if (x - 1) not in fish_tmp.keys() else fish_tmp[x - 1] + fish[x]
+    fish = fish_tmp
+
+
+print(sum(fish.values()))
