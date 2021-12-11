6a29760813e99ff3fbff143b46b6c72389ae7d26
Janeczenko <74876052+Janeczenko@users.noreply.github.com>
2021-12-11 12:36:50 +0100

    Day 9 part 2 completed

--- 2021/09/2.py
+++ 2021/09/2.py
@@ -2,41 +2,48 @@ import itertools
 
 def check_adj(x, y):
     add_nums = []
-    if ((data[x-1][y] not in ('X', 0)) if x != 0 else False):
-        add_nums.append(data[x-1][y])
     if ((data[x][y-1] not in ('X', 0)) if y != 0 else False):
         add_nums.append(data[x][y-1])
-    if ((data[x+1][y] not in ('X', 0)) if x != len(data[0]) else False):
-        add_nums.append(data[x+1][y])
-    if ((data[x][y+1] not in ('X', 0)) if y != len(data) else False):
-        add_nums.append(data[x][y+1])
+    if ((data[x-1][y] not in ('X', 0)) if x != 0 else False):
+        add_nums.append(data[x-1][y])
     return add_nums
 
 def replace_num(a, b):
-    for x, y in itertools.product(range(len(data[0])), range(len(data))):
-        pass
-    # TODO: WRITE THE REST OF IT
+    for x, y in itertools.product(range(len(data)), range(len(data[0]))):
+        if(data[x][y] == b):
+            data[x][y] = a
+
 
 with open("2021/09/input.txt", "r") as f:
     data = [[int(y) for y in x] for x in f.read().strip().splitlines()]
 
-basins = []
-
-for x, y in itertools.product(range(len(data[0])), range(len(data))):
+for x, y in itertools.product(range(len(data)), range(len(data[0]))):
     data[x][y] = 0 if data[x][y] != 9 else 'X'
 
 i = 1
-for x, y in itertools.product(range(len(data[0])), range(len(data))):
+for x, y in itertools.product(range(len(data)), range(len(data[0]))):
     if data[x][y] == 'X':
         continue
     adj = check_adj(x, y)
+    adj.sort()
     if len(adj) == 0:
         data[x][y] = i
         i += 1
     elif len(adj) == 1:
-        data[x][y] = adj[1]
+        data[x][y] = adj[0]
     else:
-        for z in range(1, len(adj)):
-            replace_num(z, adj[0])
+        data[x][y] = adj[0]
+        replace_num(adj[1], adj[0])
 
-# TODO: COMPLETE THIS
+basins = {}
+for x, y in itertools.product(range(len(data)), range(len(data[0]))):
+    if data[x][y] == "X":
+        continue
+    else:
+        basins.setdefault(data[x][y], 0)
+        basins[data[x][y]] += 1
+
+basin_sizes = list(basins.values())
+basin_sizes.sort(reverse=True)
+
+print(basin_sizes[0] * basin_sizes[1] * basin_sizes[2])
