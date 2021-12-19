696f397a339d399bed3ac2ead1793e4959e9f5d5
Janeczenko <74876052+Janeczenko@users.noreply.github.com>
2021-12-19 20:40:23 +0100

    Day 12 completed

--- 2021/12/2.py
+++ 2021/12/2.py
@@ -1,19 +1,24 @@
 import itertools
 
-def can_revisit(cave):
-    return cave.isupper()
+def can_revisit(num, cave):
+    if cave == "start":
+        return False
+    elif cave.isupper():
+        return True
+    else:
+        return len(visited[num]) == len(set(visited[num]))
 
 def path_creator(num, cave):
     global path_counter
     for next_cave in paths[cave]:
-        if next_cave not in visited[num] or can_revisit(next_cave):
+        if next_cave not in visited[num] or can_revisit(num, next_cave):
             if next_cave == "end":
-                path_debug.append(visited[num])
+                debug.append(visited[num])
                 next(counter)
                 continue
             next_num = next(generator)
             visited[next_num] = visited[num].copy()
-            if not can_revisit(next_cave) or True:
+            if not next_cave.isupper():
                 visited[next_num].append(next_cave)
             path_creator(next_num, next_cave)
     else:
@@ -22,7 +27,7 @@ def path_creator(num, cave):
 with open("2021/12/input.txt", "r") as f:
     data =  [x.split("-") for x in f.read().strip().splitlines()]
 
-path_debug = []
+debug = []
 
 generator = itertools.count()
 counter = itertools.count()
