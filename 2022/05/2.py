e3d964865e44c7c6d6aa37686e889084501ea78f
Janeczenko <janek.wyrzykowski@gmail.com>
2022-12-05 19:13:16 +0100

    Day 5 completed

---
+++ 2022/05/2.py
@@ -0,0 +1,27 @@
+import re
+
+
+def main():
+    with open('input.txt', 'r') as f:
+        crates, instructions = list(re.split(r'\n[\s0123456789]+\n', str(f.read().strip('\n'))))
+
+    crates_matrix = [[] for _ in range(len(crates.split('\n')[-1].split(' ')))]
+    for i, x in enumerate(crates.split('\n')[::-1]):
+        for j, y in enumerate(range(1, len(x), 4)):
+            if x[y] != ' ':
+                crates_matrix[j].append(x[y])
+
+    instructions_matrix = [[int(y) for y in re.findall(r'\d+', x)] for x in instructions.split('\n')]
+
+    for instruction in instructions_matrix:
+        amount, origin, destination = instruction
+        origin -= 1
+        destination -= 1
+        crates_matrix[destination].extend(crates_matrix[origin][-amount:])
+        crates_matrix[origin] = crates_matrix[origin][:-amount]
+
+    print(''.join([x[-1] for x in crates_matrix]))
+
+
+if __name__ == '__main__':
+    main()
